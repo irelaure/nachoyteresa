f7ad870769d2fb3a8d0c367d9677c9073adbf99f
Guido Peralta Garcia <guidoperalta26@gmail.com>
2020-01-28 18:51:00 +0100

    creando base

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+
+  <link rel='stylesheet' href='/js/bootstrap.min.css' media='screen'>
+  <link rel='stylesheet' href='/tools/font-awesome.min.css?v=1.1' media='screen'>
+
+  <script src='/tools/jquery-1.11.2.min.js'></script>
+  <script src='/js/bootstrap.bundle.min.js'></script>
+    
+</head>
+<body>
+  
+</body>
+</html>
